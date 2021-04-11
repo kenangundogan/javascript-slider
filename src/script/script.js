7e59e768a0457874a8dd1e59846c9f3987681b5a
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-11 19:25:24 +0300

    Update script

--- src/script/script.js
+++ src/script/script.js
@@ -8,6 +8,7 @@ const Slider = function (options) {
 
     var element = options.element,
         activeItem = options.activeItem,
+        direction = options.direction,
         container = document.querySelector(element),
         wrapper = document.querySelector(`${element} .slider-wrapper`),
         items = document.querySelectorAll(`${element} .slider-item`),
@@ -16,10 +17,12 @@ const Slider = function (options) {
         pagination = document.querySelector(`${element} .pagination`),
         itemLength = items.length,
         containerWidth = container.offsetWidth,
+        containerHeight = container.offsetHeight,
         slideIndex = 1;
 
     const selector = {
         activeItem,
+        direction,
         container,
         wrapper,
         items,
@@ -28,18 +31,26 @@ const Slider = function (options) {
         pagination,
         itemLength,
         containerWidth,
+        containerHeight,
         slideIndex
     };
 
 
     function sliderFunc(selector) {
+        directionFunc(selector);
+    }
+
+    function directionFunc(selector) {
+        if (selector.direction) {
+            selector.container.classList.add(selector.direction);
+        }
         navigationFunc(selector);
     }
 
     function navigationFunc(selector) {
         selector.next.addEventListener("click", () => {
             if (selector.slideIndex < selector.itemLength) {
-                selector.wrapper.style.transform = 'translate3d(-' + (selector.containerWidth * selector.slideIndex) + 'px,0,0)';
+                navigationNextFunc(selector);
                 selector.slideIndex++;
             }
         });
@@ -47,11 +58,27 @@ const Slider = function (options) {
         selector.prev.addEventListener("click", () => {
             if (selector.slideIndex > 1) {
                 selector.slideIndex--;
-                selector.wrapper.style.transform = 'translate3d(-' + (selector.containerWidth * (selector.slideIndex - 1)) + 'px,0,0)';
+                navigationPrevFunc(selector);
             }
         });
     }
 
+    function navigationNextFunc(selector) {
+        if (selector.direction != "vertical") {
+            selector.wrapper.style.transform = 'translate3d(-' + (selector.containerWidth * selector.slideIndex) + 'px,0,0)';
+        }
+        else {
+            selector.wrapper.style.transform = 'translate3d(0, -' + (selector.containerHeight * selector.slideIndex) + 'px,0)';
+        }
+    }
+    function navigationPrevFunc(selector) {
+        if (selector.direction != "vertical") {
+            selector.wrapper.style.transform = 'translate3d(-' + (selector.containerWidth * (selector.slideIndex - 1)) + 'px,0,0)';
+        }
+        else {
+            selector.wrapper.style.transform = 'translate3d(0, -' + (selector.containerHeight * (selector.slideIndex - 1)) + 'px,0)';
+        }
+    }
 
     sliderFunc(selector);
 
