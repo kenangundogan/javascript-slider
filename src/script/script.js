fbc5b51177d00f330d954a1d42b3319dbac32c55
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-13 21:03:46 +0300

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -6,98 +6,103 @@
 
 const Slider = function (options) {
 
-    const element = options.element,
+    const
+        element = options.element,
         activeItem = options.activeItem,
         direction = options.direction,
-        container = document.querySelector(element),
-        wrapper = document.querySelector(`${element} .slider-wrapper`),
-        items = document.querySelectorAll(`${element} .slider-item`),
-        next = document.querySelector(`${element} .navigation.next`),
-        prev = document.querySelector(`${element} .navigation.prev`),
-        pagination = document.querySelector(`${element} .pagination`),
-        itemLength = items.length,
-        containerWidth = container.offsetWidth,
-        containerHeight = container.offsetHeight,
+        pagination = options.pagination,
+        navigation = options.navigation,
+        containerSlctr = document.querySelector(element),
+        wrapperSlctr = document.querySelector(`${element} .slider-wrapper`),
+        itemsSlctr = document.querySelectorAll(`${element} .slider-item`),
+        nextSlctr = document.querySelector(`${element} .navigation.next`),
+        prevSlctr = document.querySelector(`${element} .navigation.prev`),
+        paginationSlctr = document.querySelector(`${element} .pagination`),
+        itemLength = itemsSlctr.length,
+        containerWidth = containerSlctr.offsetWidth,
+        containerHeight = containerSlctr.offsetHeight,
         slideIndex = 1;
 
-    const selector = {
+    const option = {
+        element,
         activeItem,
         direction,
-        container,
-        wrapper,
-        items,
-        next,
-        prev,
         pagination,
+        navigation,
+        containerSlctr,
+        wrapperSlctr,
+        itemsSlctr,
+        nextSlctr,
+        prevSlctr,
+        paginationSlctr,
         itemLength,
         containerWidth,
         containerHeight,
         slideIndex
     };
 
-
-    function sliderFunc(selector) {
-        directionFunc(selector);
-        paginationFunc(selector);
-    }
-
-    function paginationFunc(selector) {
-        let paginationBullet = "";
-        for (let i = 1; i < selector.itemLength + 1; i++) {
-            paginationBullet += `<span class="bullet ${selector.activeItem == i ? "active" : ""}"></span>`;
+    function paginationFunc(option) {
+        if (option.pagination && option.paginationSlctr) {
+            let paginationBullet = "";
+            for (let i = 1; i < option.itemLength + 1; i++) {
+                paginationBullet += `<span class="bullet ${option.activeItem == i ? "active" : ""}"></span>`;
+            }
+            option.paginationSlctr.innerHTML = paginationBullet;
         }
-        selector.pagination.innerHTML = paginationBullet;
     }
 
-    function directionFunc(selector) {
-        if (selector.direction) {
-            selector.container.classList.add(selector.direction);
+    function directionFunc(option) {
+        if (option.direction) {
+            option.containerSlctr.classList.add(option.direction);
         }
-        navigationFunc(selector);
+        else{
+            option.containerSlctr.classList.add("horizontal");
+        }
+        navigationFunc(option);
     }
 
-    function navigationFunc(selector) {
-        selector.next.addEventListener("click", () => {
-            if (selector.slideIndex < selector.itemLength) {
-                navigationNextFunc(selector);
-                currentItemFunc(selector);
-                currentPaginationFunc(selector);
-                selector.slideIndex++;
+    function navigationFunc(option) {
+        option.nextSlctr.addEventListener("click", () => {
+            if (option.slideIndex < option.itemLength) {
+                navigationNextFunc(option);
+                currentItemFunc(option);
+                currentPaginationFunc(option);
+                option.slideIndex++;
             }
         });
 
-        selector.prev.addEventListener("click", () => {
-            if (selector.slideIndex > 1) {
-                selector.slideIndex--;
+        option.prevSlctr.addEventListener("click", () => {
+            if (option.slideIndex > 1) {
+                option.slideIndex--;
                 prevIndex = 1;
-                currentItemFunc(selector, prevIndex);
-                currentPaginationFunc(selector, prevIndex);
-                navigationPrevFunc(selector);
+                currentItemFunc(option, prevIndex);
+                currentPaginationFunc(option, prevIndex);
+                navigationPrevFunc(option);
             }
         });
     }
 
-    function navigationNextFunc(selector) {
-        if (selector.direction != "vertical") {
-            selector.wrapper.style.transform = `translate3d(-${selector.containerWidth * selector.slideIndex}px,0,0)`;
+    function navigationNextFunc(option) {
+        if (option.direction != "vertical") {
+            option.wrapperSlctr.style.transform = `translate3d(-${option.containerWidth * option.slideIndex}px,0,0)`;
         }
         else {
-            selector.wrapper.style.transform = `translate3d(0, -${selector.containerHeight * selector.slideIndex}px,0)`;
+            option.wrapperSlctr.style.transform = `translate3d(0, -${option.containerHeight * option.slideIndex}px,0)`;
         }
     }
-    function navigationPrevFunc(selector) {
-        if (selector.direction != "vertical") {
-            selector.wrapper.style.transform = `translate3d(-${selector.containerWidth * (selector.slideIndex - 1)}px,0,0)`;
+    function navigationPrevFunc(option) {
+        if (option.direction != "vertical") {
+            option.wrapperSlctr.style.transform = `translate3d(-${option.containerWidth * (option.slideIndex - 1)}px,0,0)`;
         }
         else {
-            selector.wrapper.style.transform = `translate3d(0, -${selector.containerHeight * (selector.slideIndex - 1)}px,0)`;
+            option.wrapperSlctr.style.transform = `translate3d(0, -${option.containerHeight * (option.slideIndex - 1)}px,0)`;
         }
     }
 
-    function currentItemFunc(selector, prevIndex) {
-        selector.items.forEach((item, key) => {
+    function currentItemFunc(option, prevIndex) {
+        option.itemsSlctr.forEach((item, key) => {
             prevIndex != null ? (key = key + prevIndex) : key;
-            if (selector.slideIndex === key) {
+            if (option.slideIndex === key) {
                 item.classList.add("active");
             }
             else {
@@ -106,19 +111,26 @@ const Slider = function (options) {
         });
     }
 
-    function currentPaginationFunc(selector, prevIndex) {
-        selector.pagination.querySelectorAll(".bullet").forEach((bullet, key) => {
-            prevIndex != null ? (key = key + prevIndex) : key;
-            if (selector.slideIndex === key) {
-                bullet.classList.add("active");
-            }
-            else {
-                bullet.classList.remove("active");
-            }
-        });
+    function currentPaginationFunc(option, prevIndex) {
+        if (option.paginationSlctr) {
+            option.paginationSlctr.querySelectorAll(".bullet").forEach((bullet, key) => {
+                prevIndex != null ? (key = key + prevIndex) : key;
+                if (option.slideIndex === key) {
+                    bullet.classList.add("active");
+                }
+                else {
+                    bullet.classList.remove("active");
+                }
+            });
+        }
     }
 
-    sliderFunc(selector);
+    function sliderFunc(option) {
+        directionFunc(option);
+        paginationFunc(option);
+    }
+
+    sliderFunc(option);
 }
 
 window.Slider = Slider;
